3f579a26a6fa935a6959c9479e80c3e00fedb981
galihpermana29 <galihcbn123@gmail.com>
2021-12-26 18:53:02 +0700

    [galih] fixing layouting grid in notif and message

--- components/Profile.js
+++ components/Profile.js
@@ -3,7 +3,7 @@ import profile from '../public/assets/cards/person1.png';
 import chevron from '../public/assets/searchbar/chevron.png';
 const Profile = () => {
 	return (
-		<div className="flex items-center space-x-3 sm:space-x-0 text-white w-full sm:justify-end">
+		<div className="flex items-center space-x-3 sm:space-x-0 text-white w-full sm:justify-center">
 			<div className="">
 				<Image src={profile} alt={'Profile'} width={40} height={40} />
 			</div>
--- components/SecondaryBar.js
+++ components/SecondaryBar.js
@@ -8,11 +8,11 @@ import menu from '../public/assets/searchbar/menu.png';
 
 const SecondaryBar = () => {
 	return (
-		<div className="sm:ml-0 ml-[140px] grid grid-cols-6 h-fit gap-6 sm:gap-3 sm:mt-6">
-			<div className="sm:col-span-1 sm:flex sm:justify-center sm:items-center hidden sm:visible sm:w-full">
+		<div className="sm:ml-0 ml-[140px] grid grid-cols-6 sm:grid-cols-[40px_1fr_1fr_1fr_1fr_1fr] h-fit gap-6 sm:gap-2 sm:mt-6">
+			<div className="sm:col-span-1 sm:flex sm:justify-center sm:items-center hidden sm:visible sm:w-full ">
 				<Image src={menu} alt={'a'} />
 			</div>
-			<div className="col-span-3 col-start-1 col-ends-3 h-[58px] sm:h-[40px] sm:col-span-3">
+			<div className="col-span-3 col-start-1 col-ends-3 h-[58px] sm:h-[40px] sm:col-span-2">
 				<Searchbar />
 			</div>
 
@@ -20,7 +20,19 @@ const SecondaryBar = () => {
 				<Money />
 			</div>
 
-			<div className="col-span-2 sm:col-span-2 h-[58px] sm:h-[40px] items-center grid grid-cols-[40px_40px_minmax(40px,1fr)] gap-6 sm:gap-3 ">
+			<div className="col-span-2 sm:col-span-3 h-[58px] sm:h-[40px] sm:grid items-center sm:grid-cols-[repeat(3,minmax(40px,1fr))] sm:gap-2 hidden ">
+				<div className="sm:col-span-1 sm:flex sm:justify-center">
+					<Message />
+				</div>
+				<div className="sm:col-span-1 sm:flex sm:justify-center">
+					<Notification />
+				</div>
+				<div className="sm:col-span-1 sm:flex sm:justify-center">
+					<Profile />
+				</div>
+			</div>
+			{/* desktop */}
+			<div className="col-span-2 sm:col-span-2 h-[58px] sm:h-[40px] items-center grid grid-cols-[40px_40px_minmax(40px,1fr)] gap-6 sm:gap-3 sm:hidden ">
 				<Message />
 				<Notification />
 				<Profile />
